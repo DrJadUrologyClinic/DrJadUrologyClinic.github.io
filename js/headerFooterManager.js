4d126f790d46f9f5919fb20b091b00088513904c
Jadsmadi81 <Jadalsmadi@outlook.com>
2024-10-02 16:02:18 +0300

    og img content edit

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -11,7 +11,7 @@ class SpecialHeader extends HTMLElement {
   <title>عيادة الدكتور جاد الصمادي </title>
   <link rel="icon" type="image/DRJADALSMADI-Icon" href="images/favicon.ico">
   <meta property="og:title" content="Dr. Jad Alsmadi Urology Clinic" />
-  <meta property="og:description" content="The best urologist in Amman Jordan" />
+  <meta property="og:description" content="Dr. Jad Alsmadi Urology Clinic - DJUC Amman Jordan" />
   <meta property="og:image" content="https://drjadurologyclinic.com/images/ogimage.jpg" />
   <meta property="og:url" content="https://drjadurologyclinic.com/" />
   <meta property="og:type" content="website" />
