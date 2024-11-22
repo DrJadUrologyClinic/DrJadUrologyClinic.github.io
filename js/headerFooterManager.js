7a307ad22a222bb3e4ed7821c26c52e1cfc56c1a
Jadsmadi81 <Jadalsmadi@outlook.com>
2024-11-22 06:56:02 +0300

    Update headerFooterManager.js

--- js/headerFooterManager.js
+++ js/headerFooterManager.js
@@ -346,6 +346,11 @@ class SpecialFooter extends HTMLElement {
     this.innerHTML = `
 <!DOCTYPE html>
 <html dir="rtl" lang="ar-Jo">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
   
 <footer>
   <div class="content">
